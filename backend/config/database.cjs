7ffddcc9dd0f4168fa0d0a9565169b8204748e08
Sakhila Thami <sakhthani@gmail.com>
2026-07-14 15:42:46 +0545

    update in database.cjs

--- backend/config/database.cjs
+++ backend/config/database.cjs
@@ -12,4 +12,14 @@ module.exports = {
       },
     },
   },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
